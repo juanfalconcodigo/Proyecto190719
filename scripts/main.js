41579358d03a92d67e8ffafcd612a7cb859913e6
Juan Falcon <juancanen_10_@hotmail.com>
2019-07-26 07:30:42 -0500

    avanze presentacion 260719

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,26 @@
 'use strict'
-let buttonBarra=document.body.querySelector('#js_barra');
-        let body=document.body;
-        
-        buttonBarra.addEventListener('click',()=>{
-            console.log("gaaaaa");
-            body.classList.toggle('show-menu');
-        });
+let buttonBarra = document.body.querySelector('#js_barra');
+let body = document.body;
+
+buttonBarra.addEventListener('click', () => {
+    console.log("gaaaaa");
+    body.classList.toggle('show-menu');
+});
+
+
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+
+    return t;
+}(document, "script", "twitter-wjs"));
